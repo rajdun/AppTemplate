2795b68ed29a7cb5e7786776ff4e0e9d3566ed88
xdrpl <xdr@xdr.xdr>
2025-10-20 21:12:45 +0200

    feat: add conditional authorization for Hangfire dashboard in debug mode

--- Api/Common/HangfireAuthorizationFilter.cs
+++ Api/Common/HangfireAuthorizationFilter.cs
@@ -6,6 +6,10 @@ public class HangfireAuthorizationFilter : IDashboardAuthorizationFilter
 {
     public bool Authorize(DashboardContext context)
     {
+#if DEBUG
         return true;
+#else
+        return context.GetHttpContext().User.Identity?.IsAuthenticated ?? false;
+#endif
     }
 }
